f3eccf1b4a35bed926dfffc3e812ce17d6f3b924
Om Aditya Pandey <ompandey690@gmail.com>
2026-01-27 22:53:40 +0530

    27
    Remove Element

---
+++ Two Pointers/27.cpp
@@ -0,0 +1,27 @@
+// 27. Remove Element
+// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+// Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+// Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+// Return k.
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        vector<bool>check(nums.size(),true);
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==val){
+                check[i]=false;
+            }
+        }
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+            if(check[i]){
+                ans.push_back(nums[i]);
+            }
+        }
+       nums=ans;
+       return nums.size();
+    }
+};
